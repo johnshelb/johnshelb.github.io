493118d5717be8b0464d18860f7d5af7db3ef5fa
johnshelb <jsnd11@gmail.com>
2020-07-28 19:06:13 -0400

    enter key activates search button when input focused

--- LaCelians/script.js
+++ LaCelians/script.js
@@ -6,6 +6,15 @@ window.onload=(()=>{
   input = document.getElementsByTagName("input")[0];
   input.value="";
   input.addEventListener("click",reset);
+  input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
 })
 
 
